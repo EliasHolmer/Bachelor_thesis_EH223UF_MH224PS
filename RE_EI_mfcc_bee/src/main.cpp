0b7efad8eead1748a8e98afa990604bb0155a0f6
Mattejh <mh224ps@student.lnu.se>
2022-04-28 17:26:20 +0200

    last edit, add rfft

--- RE_EI_mfcc_bee/src/main.cpp
+++ RE_EI_mfcc_bee/src/main.cpp
@@ -29,14 +29,18 @@ constexpr int kTensorArenaSize = 2000;
 uint8_t tensor_arena[kTensorArenaSize];
 }  // namespace
 
+static inference_t inference;
+static signed short sampleBuffer[2048];
+static bool debug_nn = true; // Set this to true to see e.g. features generated from the raw signal
+
 #define EI_DSP_MATRIX(name, ...) matrix_t name(__VA_ARGS__); if (!name.buffer) { -1; }
 #define EI_DSP_MATRIX_B(name, ...) matrix_t name(__VA_ARGS__); if (!name.buffer) { -1; }
 
 const char* classifier_inferencing_categories[] = { "Bee", "notBee" };
 
-uint8_t ei_dsp_config_55_axes[] = { 0 };
-const uint32_t ei_dsp_config_55_axes_size = 1;
-ei_dsp_config_mfcc_t ei_dsp_config_55 = {
+uint8_t dsp_config_55_axes[] = { 0 };
+const uint32_t dsp_config_55_axes_size = 1;
+dsp_config_mfcc_t dsp_config_55 = {
     2,
     1,
     13,
@@ -56,9 +60,9 @@ model_dsp_t dsp_blocks[dsp_blocks_size] = {
     { // DSP block 55
         806,
         &extract_mfcc_features,
-        (void*)&ei_dsp_config_55,
-        ei_dsp_config_55_axes,
-        ei_dsp_config_55_axes_size
+        (void*)&dsp_config_55,
+        dsp_config_55_axes,
+        dsp_config_55_axes_size
     }
 };
 
@@ -83,7 +87,7 @@ typedef struct {
     int high_frequency;
     float pre_cof;
     int pre_shift;
-} ei_dsp_config_mfcc_t;
+} dsp_config_mfcc_t;
 
 typedef struct {
     size_t n_output_features;
@@ -151,7 +155,7 @@ typedef struct ei_signal_t {
 typedef struct {
     const char *label;
     float value;
-} ei_impulse_result_classification_t;
+} result_classification_t;
 
 typedef struct {
     int sampling;
@@ -161,12 +165,12 @@ typedef struct {
     int64_t dsp_us;
     int64_t classification_us;
     int64_t anomaly_us;
-} ei_impulse_result_timing_t;
+} result_timing_t;
 
 typedef struct {
-    ei_impulse_result_classification_t classification[2];
+    result_classification_t classification[2];
     float anomaly;
-    ei_impulse_result_timing_t timing;
+    result_timing_t timing;
     int32_t label_detected;
 } result_t;
 
@@ -178,9 +182,340 @@ typedef struct {
     uint32_t n_samples;
 } inference_t;
 
-static inference_t inference;
-static signed short sampleBuffer[2048];
-static bool debug_nn = true; // Set this to true to see e.g. features generated from the raw signal
+   /**
+   * @brief 32-bit floating-point type definition.
+   */
+  typedef float float32_t;
+
+ /**
+  @par
+  Example code for Floating-point Twiddle factors Generation:
+  @par
+  <pre>for (i = 0; i< N/; i++)
+  {
+ 	twiddleCoef[2*i]   = cos(i * 2*PI/(float)N);
+ 	twiddleCoef[2*i+1] = sin(i * 2*PI/(float)N);
+  } </pre>
+  @par
+  where N = 128, PI = 3.14159265358979
+  @par
+  Cos and Sin values are in interleaved fashion
+*/
+const float32_t twiddleCoef_128[256] = {
+    1.000000000f,  0.000000000f,
+    0.998795456f,  0.049067674f,
+    0.995184727f,  0.098017140f,
+    0.989176510f,  0.146730474f,
+    0.980785280f,  0.195090322f,
+    0.970031253f,  0.242980180f,
+    0.956940336f,  0.290284677f,
+    0.941544065f,  0.336889853f,
+    0.923879533f,  0.382683432f,
+    0.903989293f,  0.427555093f,
+    0.881921264f,  0.471396737f,
+    0.857728610f,  0.514102744f,
+    0.831469612f,  0.555570233f,
+    0.803207531f,  0.595699304f,
+    0.773010453f,  0.634393284f,
+    0.740951125f,  0.671558955f,
+    0.707106781f,  0.707106781f,
+    0.671558955f,  0.740951125f,
+    0.634393284f,  0.773010453f,
+    0.595699304f,  0.803207531f,
+    0.555570233f,  0.831469612f,
+    0.514102744f,  0.857728610f,
+    0.471396737f,  0.881921264f,
+    0.427555093f,  0.903989293f,
+    0.382683432f,  0.923879533f,
+    0.336889853f,  0.941544065f,
+    0.290284677f,  0.956940336f,
+    0.242980180f,  0.970031253f,
+    0.195090322f,  0.980785280f,
+    0.146730474f,  0.989176510f,
+    0.098017140f,  0.995184727f,
+    0.049067674f,  0.998795456f,
+    0.000000000f,  1.000000000f,
+   -0.049067674f,  0.998795456f,
+   -0.098017140f,  0.995184727f,
+   -0.146730474f,  0.989176510f,
+   -0.195090322f,  0.980785280f,
+   -0.242980180f,  0.970031253f,
+   -0.290284677f,  0.956940336f,
+   -0.336889853f,  0.941544065f,
+   -0.382683432f,  0.923879533f,
+   -0.427555093f,  0.903989293f,
+   -0.471396737f,  0.881921264f,
+   -0.514102744f,  0.857728610f,
+   -0.555570233f,  0.831469612f,
+   -0.595699304f,  0.803207531f,
+   -0.634393284f,  0.773010453f,
+   -0.671558955f,  0.740951125f,
+   -0.707106781f,  0.707106781f,
+   -0.740951125f,  0.671558955f,
+   -0.773010453f,  0.634393284f,
+   -0.803207531f,  0.595699304f,
+   -0.831469612f,  0.555570233f,
+   -0.857728610f,  0.514102744f,
+   -0.881921264f,  0.471396737f,
+   -0.903989293f,  0.427555093f,
+   -0.923879533f,  0.382683432f,
+   -0.941544065f,  0.336889853f,
+   -0.956940336f,  0.290284677f,
+   -0.970031253f,  0.242980180f,
+   -0.980785280f,  0.195090322f,
+   -0.989176510f,  0.146730474f,
+   -0.995184727f,  0.098017140f,
+   -0.998795456f,  0.049067674f,
+   -1.000000000f,  0.000000000f,
+   -0.998795456f, -0.049067674f,
+   -0.995184727f, -0.098017140f,
+   -0.989176510f, -0.146730474f,
+   -0.980785280f, -0.195090322f,
+   -0.970031253f, -0.242980180f,
+   -0.956940336f, -0.290284677f,
+   -0.941544065f, -0.336889853f,
+   -0.923879533f, -0.382683432f,
+   -0.903989293f, -0.427555093f,
+   -0.881921264f, -0.471396737f,
+   -0.857728610f, -0.514102744f,
+   -0.831469612f, -0.555570233f,
+   -0.803207531f, -0.595699304f,
+   -0.773010453f, -0.634393284f,
+   -0.740951125f, -0.671558955f,
+   -0.707106781f, -0.707106781f,
+   -0.671558955f, -0.740951125f,
+   -0.634393284f, -0.773010453f,
+   -0.595699304f, -0.803207531f,
+   -0.555570233f, -0.831469612f,
+   -0.514102744f, -0.857728610f,
+   -0.471396737f, -0.881921264f,
+   -0.427555093f, -0.903989293f,
+   -0.382683432f, -0.923879533f,
+   -0.336889853f, -0.941544065f,
+   -0.290284677f, -0.956940336f,
+   -0.242980180f, -0.970031253f,
+   -0.195090322f, -0.980785280f,
+   -0.146730474f, -0.989176510f,
+   -0.098017140f, -0.995184727f,
+   -0.049067674f, -0.998795456f,
+   -0.000000000f, -1.000000000f,
+    0.049067674f, -0.998795456f,
+    0.098017140f, -0.995184727f,
+    0.146730474f, -0.989176510f,
+    0.195090322f, -0.980785280f,
+    0.242980180f, -0.970031253f,
+    0.290284677f, -0.956940336f,
+    0.336889853f, -0.941544065f,
+    0.382683432f, -0.923879533f,
+    0.427555093f, -0.903989293f,
+    0.471396737f, -0.881921264f,
+    0.514102744f, -0.857728610f,
+    0.555570233f, -0.831469612f,
+    0.595699304f, -0.803207531f,
+    0.634393284f, -0.773010453f,
+    0.671558955f, -0.740951125f,
+    0.707106781f, -0.707106781f,
+    0.740951125f, -0.671558955f,
+    0.773010453f, -0.634393284f,
+    0.803207531f, -0.595699304f,
+    0.831469612f, -0.555570233f,
+    0.857728610f, -0.514102744f,
+    0.881921264f, -0.471396737f,
+    0.903989293f, -0.427555093f,
+    0.923879533f, -0.382683432f,
+    0.941544065f, -0.336889853f,
+    0.956940336f, -0.290284677f,
+    0.970031253f, -0.242980180f,
+    0.980785280f, -0.195090322f,
+    0.989176510f, -0.146730474f,
+    0.995184727f, -0.098017140f,
+    0.998795456f, -0.049067674f
+};
+
+const uint16_t armBitRevIndexTable128[((uint16_t)208)] =
+{
+   /* 8x2, size 208 */
+   8,512, 16,64, 24,576, 32,128, 40,640, 48,192, 56,704, 64,256, 72,768,
+   80,320, 88,832, 96,384, 104,896, 112,448, 120,960, 128,512, 136,520,
+   144,768, 152,584, 160,520, 168,648, 176,200, 184,712, 192,264, 200,776,
+   208,328, 216,840, 224,392, 232,904, 240,456, 248,968, 264,528, 272,320,
+   280,592, 288,768, 296,656, 304,328, 312,720, 328,784, 344,848, 352,400,
+   360,912, 368,464, 376,976, 384,576, 392,536, 400,832, 408,600, 416,584,
+   424,664, 432,840, 440,728, 448,592, 456,792, 464,848, 472,856, 480,600,
+   488,920, 496,856, 504,984, 520,544, 528,576, 536,608, 552,672, 560,608,
+   568,736, 576,768, 584,800, 592,832, 600,864, 608,800, 616,928, 624,864,
+   632,992, 648,672, 656,896, 664,928, 688,904, 696,744, 704,896, 712,808,
+   720,912, 728,872, 736,928, 744,936, 752,920, 760,1000, 776,800, 784,832,
+   792,864, 808,904, 816,864, 824,920, 840,864, 856,880, 872,944, 888,1008,
+   904,928, 912,960, 920,992, 944,968, 952,1000, 968,992, 984,1008
+};
+
+const float32_t twiddleCoef_rfft_256[256] = {
+    0.000000000f,  1.000000000f,
+    0.024541229f,  0.999698819f,
+    0.049067674f,  0.998795456f,
+    0.073564564f,  0.997290457f,
+    0.098017140f,  0.995184727f,
+    0.122410675f,  0.992479535f,
+    0.146730474f,  0.989176510f,
+    0.170961889f,  0.985277642f,
+    0.195090322f,  0.980785280f,
+    0.219101240f,  0.975702130f,
+    0.242980180f,  0.970031253f,
+    0.266712757f,  0.963776066f,
+    0.290284677f,  0.956940336f,
+    0.313681740f,  0.949528181f,
+    0.336889853f,  0.941544065f,
+    0.359895037f,  0.932992799f,
+    0.382683432f,  0.923879533f,
+    0.405241314f,  0.914209756f,
+    0.427555093f,  0.903989293f,
+    0.449611330f,  0.893224301f,
+    0.471396737f,  0.881921264f,
+    0.492898192f,  0.870086991f,
+    0.514102744f,  0.857728610f,
+    0.534997620f,  0.844853565f,
+    0.555570233f,  0.831469612f,
+    0.575808191f,  0.817584813f,
+    0.595699304f,  0.803207531f,
+    0.615231591f,  0.788346428f,
+    0.634393284f,  0.773010453f,
+    0.653172843f,  0.757208847f,
+    0.671558955f,  0.740951125f,
+    0.689540545f,  0.724247083f,
+    0.707106781f,  0.707106781f,
+    0.724247083f,  0.689540545f,
+    0.740951125f,  0.671558955f,
+    0.757208847f,  0.653172843f,
+    0.773010453f,  0.634393284f,
+    0.788346428f,  0.615231591f,
+    0.803207531f,  0.595699304f,
+    0.817584813f,  0.575808191f,
+    0.831469612f,  0.555570233f,
+    0.844853565f,  0.534997620f,
+    0.857728610f,  0.514102744f,
+    0.870086991f,  0.492898192f,
+    0.881921264f,  0.471396737f,
+    0.893224301f,  0.449611330f,
+    0.903989293f,  0.427555093f,
+    0.914209756f,  0.405241314f,
+    0.923879533f,  0.382683432f,
+    0.932992799f,  0.359895037f,
+    0.941544065f,  0.336889853f,
+    0.949528181f,  0.313681740f,
+    0.956940336f,  0.290284677f,
+    0.963776066f,  0.266712757f,
+    0.970031253f,  0.242980180f,
+    0.975702130f,  0.219101240f,
+    0.980785280f,  0.195090322f,
+    0.985277642f,  0.170961889f,
+    0.989176510f,  0.146730474f,
+    0.992479535f,  0.122410675f,
+    0.995184727f,  0.098017140f,
+    0.997290457f,  0.073564564f,
+    0.998795456f,  0.049067674f,
+    0.999698819f,  0.024541229f,
+    1.000000000f,  0.000000000f,
+    0.999698819f, -0.024541229f,
+    0.998795456f, -0.049067674f,
+    0.997290457f, -0.073564564f,
+    0.995184727f, -0.098017140f,
+    0.992479535f, -0.122410675f,
+    0.989176510f, -0.146730474f,
+    0.985277642f, -0.170961889f,
+    0.980785280f, -0.195090322f,
+    0.975702130f, -0.219101240f,
+    0.970031253f, -0.242980180f,
+    0.963776066f, -0.266712757f,
+    0.956940336f, -0.290284677f,
+    0.949528181f, -0.313681740f,
+    0.941544065f, -0.336889853f,
+    0.932992799f, -0.359895037f,
+    0.923879533f, -0.382683432f,
+    0.914209756f, -0.405241314f,
+    0.903989293f, -0.427555093f,
+    0.893224301f, -0.449611330f,
+    0.881921264f, -0.471396737f,
+    0.870086991f, -0.492898192f,
+    0.857728610f, -0.514102744f,
+    0.844853565f, -0.534997620f,
+    0.831469612f, -0.555570233f,
+    0.817584813f, -0.575808191f,
+    0.803207531f, -0.595699304f,
+    0.788346428f, -0.615231591f,
+    0.773010453f, -0.634393284f,
+    0.757208847f, -0.653172843f,
+    0.740951125f, -0.671558955f,
+    0.724247083f, -0.689540545f,
+    0.707106781f, -0.707106781f,
+    0.689540545f, -0.724247083f,
+    0.671558955f, -0.740951125f,
+    0.653172843f, -0.757208847f,
+    0.634393284f, -0.773010453f,
+    0.615231591f, -0.788346428f,
+    0.595699304f, -0.803207531f,
+    0.575808191f, -0.817584813f,
+    0.555570233f, -0.831469612f,
+    0.534997620f, -0.844853565f,
+    0.514102744f, -0.857728610f,
+    0.492898192f, -0.870086991f,
+    0.471396737f, -0.881921264f,
+    0.449611330f, -0.893224301f,
+    0.427555093f, -0.903989293f,
+    0.405241314f, -0.914209756f,
+    0.382683432f, -0.923879533f,
+    0.359895037f, -0.932992799f,
+    0.336889853f, -0.941544065f,
+    0.313681740f, -0.949528181f,
+    0.290284677f, -0.956940336f,
+    0.266712757f, -0.963776066f,
+    0.242980180f, -0.970031253f,
+    0.219101240f, -0.975702130f,
+    0.195090322f, -0.980785280f,
+    0.170961889f, -0.985277642f,
+    0.146730474f, -0.989176510f,
+    0.122410675f, -0.992479535f,
+    0.098017140f, -0.995184727f,
+    0.073564564f, -0.997290457f,
+    0.049067674f, -0.998795456f,
+    0.024541229f, -0.999698819f
+};
+  /**
+   * @brief Instance structure for the floating-point CFFT/CIFFT function.
+   */
+  typedef struct
+  {
+          uint16_t fftLen;                   /**< length of the FFT. */
+    const float32_t *pTwiddle;         /**< points to the Twiddle factor table. */
+    const uint16_t *pBitRevTable;      /**< points to the bit reversal table. */
+          uint16_t bitRevLength;             /**< bit reversal table length. */
+  } arm_cfft_instance_f32;
+
+   const arm_cfft_instance_f32 arm_cfft_sR_f32_len128 = {
+  128, twiddleCoef_128, armBitRevIndexTable128, ((uint16_t)208)
+};
+    
+    /**
+   * @brief Instance structure for the floating-point CFFT/CIFFT function.
+   */
+  typedef struct
+  {
+          uint16_t fftLen;                   /**< length of the FFT. */
+    const float32_t *pTwiddle;         /**< points to the Twiddle factor table. */
+    const uint16_t *pBitRevTable;      /**< points to the bit reversal table. */
+          uint16_t bitRevLength;             /**< bit reversal table length. */
+
+  } arm_cfft_instance_f32;
+
+    /**
+   * @brief Instance structure for the floating-point RFFT/RIFFT function.
+   */
+typedef struct
+  {
+          arm_cfft_instance_f32 Sint;      /**< Internal CFFT structure. */
+          uint16_t fftLenRFFT;             /**< length of the real sequence */
+    const float32_t * pTwiddleRFFT;        /**< Twiddle factors real stage  */
+  } arm_rfft_fast_instance_f32 ;
 
 /**
  * Print wrapper around printf()
@@ -245,6 +580,135 @@ public:
         return 0;
     }
 
+    /**
+     * Compute the one-dimensional discrete Fourier Transform for real input.
+     * This function computes the one-dimensional n-point discrete Fourier Transform (DFT) of
+     * a real-valued array by means of an efficient algorithm called the Fast Fourier Transform (FFT).
+     * @param src Source buffer
+     * @param src_size Size of the source buffer
+     * @param output Output buffer
+     * @param output_size Size of the output buffer, should be n_fft / 2 + 1
+     * @returns 0 if OK
+     */
+    static int rfft(const float *src, size_t src_size, float *output, size_t output_size, size_t n_fft) {
+        size_t n_fft_out_features = (n_fft / 2) + 1;
+        if (output_size != n_fft_out_features) {
+            return -1;
+        }
+
+        // truncate if needed
+        if (src_size > n_fft) {
+            src_size = n_fft;
+        }
+
+        // declare input and output arrays
+        EI_DSP_MATRIX(fft_input, 1, n_fft);
+        if (!fft_input.buffer) {
+            return -1;
+        }
+
+        // copy from src to fft_input
+        memcpy(fft_input.buffer, src, src_size * sizeof(float));
+        // pad to the rigth with zeros
+        memset(fft_input.buffer + src_size, 0, (n_fft - src_size) * sizeof(float));
+
+        if (n_fft != 32 && n_fft != 64 && n_fft != 128 && n_fft != 256 &&
+            n_fft != 512 && n_fft != 1024 && n_fft != 2048 && n_fft != 4096) {
+            int ret = software_rfft(fft_input.buffer, output, n_fft, n_fft_out_features);
+            if (ret != 0) {
+                return -1;
+        }
+        else {
+            // hardware acceleration only works for the powers above...
+            arm_rfft_fast_instance_f32 rfft_instance;
+            int status = cmsis_rfft_init_f32(&rfft_instance, n_fft);
+            if (status != 0) {
+                return status;
+            }
+
+            EI_DSP_MATRIX(fft_output, 1, n_fft);
+            if (!fft_output.buffer) {
+                return -1;
+            }
+
+            arm_rfft_fast_f32(&rfft_instance, fft_input.buffer, fft_output.buffer, 0);
+
+            output[0] = fft_output.buffer[0];
+            output[n_fft_out_features - 1] = fft_output.buffer[1];
+
+            size_t fft_output_buffer_ix = 2;
+            for (size_t ix = 1; ix < n_fft_out_features - 1; ix += 1) {
+                float rms_result;
+                arm_rms_f32(fft_output.buffer + fft_output_buffer_ix, 2, &rms_result);
+                output[ix] = rms_result * sqrt(2);
+
+                fft_output_buffer_ix += 2;
+            }
+        }
+
+        return 0;
+    }
+  
+}
+    private:
+
+    
+    static int software_rfft(float *fft_input, float *output, size_t n_fft, size_t n_fft_out_features) {
+        kiss_fft_cpx *fft_output = (kiss_fft_cpx*)ei_dsp_malloc(n_fft_out_features * sizeof(kiss_fft_cpx));
+        if (!fft_output) {
+            return -1;
+        }
+
+        size_t kiss_fftr_mem_length;
+
+        // create fftr context
+        kiss_fftr_cfg cfg = kiss_fftr_alloc(n_fft, 0, NULL, NULL, &kiss_fftr_mem_length);
+        if (!cfg) {
+            ei_dsp_free(fft_output, n_fft_out_features * sizeof(kiss_fft_cpx));
+            EIDSP_ERR(EIDSP_OUT_OF_MEM);
+        }
+
+        ei_dsp_register_alloc(kiss_fftr_mem_length, cfg);
+
+        // execute the rfft operation
+        kiss_fftr(cfg, fft_input, fft_output);
+
+        // and write back to the output
+        for (size_t ix = 0; ix < n_fft_out_features; ix++) {
+            output[ix] = sqrt(pow(fft_output[ix].r, 2) + pow(fft_output[ix].i, 2));
+        }
+
+        ei_dsp_free(cfg, kiss_fftr_mem_length);
+        ei_dsp_free(fft_output, n_fft_out_features * sizeof(kiss_fft_cpx));
+
+        return EIDSP_OK;
+    }
+
+     /**
+     * Initialize a CMSIS-DSP fast rfft structure
+     * We do it this way as this means we can compile out fast_init calls which hints the compiler
+     * to which tables can be removed
+     */
+    static int cmsis_rfft_init_f32(arm_rfft_fast_instance_f32 *rfft_instance, const size_t n_fft)
+    {
+// ARM cores (ex M55) with Helium extensions (MVEF) need special treatment (Issue 2843)
+
+        switch (n_fft) {
+            case 256: {
+                arm_cfft_instance_f32 *S = &(rfft_instance->Sint);
+                S->fftLen = 128U;
+                S->pTwiddle = NULL;
+                S->bitRevLength = arm_cfft_sR_f32_len128.bitRevLength;
+                S->pBitRevTable = arm_cfft_sR_f32_len128.pBitRevTable;
+                S->pTwiddle = arm_cfft_sR_f32_len128.pTwiddle;
+                rfft_instance->fftLenRFFT = 256U;
+                rfft_instance->pTwiddleRFFT = (float32_t *) twiddleCoef_rfft_256;
+                return 0;
+            }
+            default:
+                return -1;
+        }
+    }
     // more numpy code here
 };
 
@@ -1099,7 +1563,7 @@ static int preemphasized_audio_signal_get_data(size_t offset, size_t length, flo
 }
 
 __attribute__((unused)) int extract_mfcc_features(signal_t *signal, matrix_t *output_matrix, void *config_ptr, const float sampling_frequency) {
-    ei_dsp_config_mfcc_t config = *((ei_dsp_config_mfcc_t*)config_ptr);
+    dsp_config_mfcc_t config = *((dsp_config_mfcc_t*)config_ptr);
 
     if (config.axes != 1) {
         return -1;
